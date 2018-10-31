db960cb8b1933ff88b2693b307e7d35121663820
Dani Lyn <dlyn1110@gmail.com>
2018-10-31 20:12:01 +0000

    automatically backed up by learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -8,9 +8,16 @@ class RecipesController < ApplicationController
   end
 
   def new
-    @recipe = Recipe.new
-  end
+     @recipe = Recipe.new
+     @recipe.ingredients.build
+     @recipe.ingredients.build
+   end
 
-  def create
-  end
+   def create
+     @recipe = Recipe.create(recipe_params)
+     if @recipe.save
+     redirect_to recipe_path(@recipe)
+   else
+     render :new
+   end
 end
